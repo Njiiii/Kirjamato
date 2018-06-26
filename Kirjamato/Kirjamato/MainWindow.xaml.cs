12d305cce346c9ac63f289db4c9ac41b846f9b3b
Niko_Jousjärvi_1 <jousjarvi@gmail.com>
2018-06-26 15:57:07 +0300

    Work on login screen and user creation screen started

---
+++ Kirjamato/Kirjamato/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kirjamato
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Kirjaudu sisään painike
+        //Jos käyttäjän antama käyttäjänimi ja salasana ovat hyväksyttyjä, päästetään käyttäjä käyttämään ohjelmaa
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //Luo uusi käyttäjä painike
+        //Avaa uuden ikkunan jossa käyttäjä voi luoda uuden käyttäjätunnuksen
+        private void NewUserButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
